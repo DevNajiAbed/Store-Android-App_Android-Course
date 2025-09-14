4ee8fc89ccfb49d389aad5ea507e2c5a2bb03812
Naji Abed <nageaped31@gmail.com>
2025-09-14 15:24:30 +0300

    Initial commit

---
+++ app/src/main/java/com/pcit/shop/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.pcit.shop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.pcit.shop.ui.theme.ShopTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShopTheme {
+
+            }
+        }
+    }
+}
